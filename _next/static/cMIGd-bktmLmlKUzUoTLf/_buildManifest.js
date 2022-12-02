31d6d7e418bf78bc8e53b1a9c6c2aa7fbdcc67bc
Ian <ian@amond.io>
2022-12-03 00:14:59 +0900

    deploy to gh-pages

--- _next/static/cMIGd-bktmLmlKUzUoTLf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/90-cac9edd53d6d588834d7.js","static/chunks/pages/index-3343250bc4999ba48afa.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
